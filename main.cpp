2eafb77a65d090ade1d7a34de7470963fe5b38dc
Den <voblagf@mail.ru>
2024-07-07 11:23:22 +0300

    code: add maximum

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ int main() // you may type whatever you want
 	cout << "Enter A and B:\n";
 	int a, b;
 	cin >> a >> b;
-	
+	int max = a > b ? a : b;
 	cout << "A + B = " << a + b << '\n'
 		<< "A - B = " << a - b << '\n'
 		<< "A * B = " << a * b << '\n'
-		<< "A / B = " << a / b << '\n';
+		<< "A / B = " << a / b << '\n'
+		<< "MAX: " << max << '\n';
 
 	return 0;
 }
